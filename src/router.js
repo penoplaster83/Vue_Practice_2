fa08360fa76b599729486fb3464259b00bb1be81
MACROS <penoplaster83@gmail.com>
2019-09-29 00:35:46 +0300

    1aaa

---
+++ src/router.js
@@ -0,0 +1,31 @@
+/* eslint-disable standard/object-curly-even-spacing */
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+
+    {
+      path: '/login',
+      name: 'login',
+      meta: { layout: 'empty' },
+      component: () => import('./views/Login.vue')
+    },
+    {
+      path: '/categories',
+      name: 'categories',
+      meta: { layout: 'main' },
+      component: () => import('./views/Categories.vue')
+    }
+  ]
+})
