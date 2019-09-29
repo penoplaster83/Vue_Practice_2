f170db6763b4a2975a0ca67738e999f48e214db3
penoplaster83 <42916143+penoplaster83@users.noreply.github.com>
2019-09-29 12:13:32 +0300

    All_Templates_Translated_To_Vue_Components

--- src/router.js
+++ src/router.js
@@ -1,31 +1,67 @@
 /* eslint-disable standard/object-curly-even-spacing */
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-
-    {
-      path: '/login',
-      name: 'login',
-      meta: { layout: 'empty' },
-      component: () => import('./views/Login.vue')
+      path: "/",
+      name: "home",
+      meta: { layout: "main" },
+      component: () => import("./views/Home.vue")
     },
     {
-      path: '/categories',
-      name: 'categories',
-      meta: { layout: 'main' },
-      component: () => import('./views/Categories.vue')
+      path: "/profile",
+      name: "profile",
+      meta: { layout: "main" },
+      component: () => import("./views/Profile.vue")
+    },
+    {
+      path: "/login",
+      name: "login",
+      meta: { layout: "empty" },
+      component: () => import("./views/Login.vue")
+    },
+    {
+      path: "/categories",
+      name: "categories",
+      meta: { layout: "main" },
+      component: () => import("./views/Categories.vue")
+    },
+    {
+      path: "/record",
+      name: "record",
+      meta: { layout: "main" },
+      component: () => import("./views/Record.vue")
+    },
+    {
+      path: "/detailrecord",
+      name: "detailrecord",
+      meta: { layout: "main" },
+      component: () => import("./views/DetailRecord.vue")
+    },
+    {
+      path: "/history",
+      name: "history",
+      meta: { layout: "main" },
+      component: () => import("./views/History.vue")
+    },
+    {
+      path: "/planning",
+      name: "planning",
+      meta: { layout: "main" },
+      component: () => import("./views/Planning.vue")
+    },
+    {
+      path: "/register",
+      name: "register",
+      meta: { layout: "empty" },
+      component: () => import("./views/Register.vue")
     }
   ]
-})
+});
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,30 +3,19 @@
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
-        <input
-            id="email"
-            type="text"
-            class="validate"
-        >
+        <input id="email" type="text" class="validate">
         <label for="email">Email</label>
         <small class="helper-text invalid">Email</small>
       </div>
       <div class="input-field">
-        <input
-            id="password"
-            type="password"
-            class="validate"
-        >
+        <input id="password" type="password" class="validate">
         <label for="password">Пароль</label>
         <small class="helper-text invalid">Password</small>
       </div>
     </div>
     <div class="card-action">
       <div>
-        <button
-            class="btn waves-effect waves-light auth-submit"
-            type="submit"
-        >
+        <button class="btn waves-effect waves-light auth-submit" type="submit">
           Войти
           <i class="material-icons right">send</i>
         </button>
@@ -39,3 +28,6 @@
     </div>
   </form>
 </template>
+
+
+
