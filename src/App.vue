02930069efb481892d621c68063f7c763ac75f24
penoplaster83 <42916143+penoplaster83@users.noreply.github.com>
2019-10-03 20:58:34 +0300

    start setting validation

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import EmptyLayout from '@/layouts/EmptyLayout'
 import MainLayout from '@/layouts/MainLayout'
 export default {
   computed: {
-    layout() {
+    layout () {
       console.log()
       return (this.$route.meta.layout || 'empty') + '-layout'
     }
@@ -21,7 +21,6 @@ export default {
   }
 }
 </script>
-
 
 <style lang="scss">
 @import '~materialize-css/dist/css/materialize.min.css';
